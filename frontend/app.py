4b0422fd9821974abea0873adb6768e3e9f92e23
Vidya <vidya@gmail.com>
2026-03-30 13:43:15 +0530

    added requirements

--- frontend/app.py
+++ frontend/app.py
@@ -1,21 +1,35 @@
 import streamlit as st
-import requests
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import json
 
 st.title("🌱 Plant Disease Detector")
+
+# Load model
+model = tf.keras.models.load_model("model/plant_model.h5")
+
+# Load labels
+with open("model/labels.json") as f:
+    labels = json.load(f)
+
+labels = {v:k for k,v in labels.items()}
+
+def preprocess(image):
+    image = image.resize((224,224))
+    img = np.array(image)/255.0
+    return np.expand_dims(img, axis=0)
 
 file = st.file_uploader("Upload a leaf image")
 
 if file:
-    st.image(file, caption="Uploaded Image", use_column_width=True)
+    st.image(file)
 
     if st.button("Predict"):
-        response = requests.post(
-            "http://127.0.0.1:8000/predict",
-            files={"file": file}
-        )
+        image = Image.open(file)
+        img = preprocess(image)
 
-        if response.status_code == 200:
-            result = response.json()
-            st.success(f"Prediction: {result['prediction']}")
-        else:
-            st.error("Error in prediction")
+        pred = model.predict(img)
+        class_id = np.argmax(pred)
+
+        st.success(f"Prediction: {labels[class_id]}")
